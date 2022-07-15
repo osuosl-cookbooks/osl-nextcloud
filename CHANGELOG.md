fddf70f71a2cbcb04fdacbaf8dd8b528e2694589
Lance Albertson <lance@osuosl.org>
2022-07-15 11:23:10 -0700

    Initial commit
    Signed-off-by: Lance Albertson <lance@osuosl.org>

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# osl-nextcloud CHANGELOG
+
+This file is used to list changes made in each version of the osl-nextcloud cookbook.
+
+## 0.1.0
+
+- Initial release
---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name              'osl-nextcloud'
+maintainer        'Oregon State University'
+maintainer_email  'chef@osuosl.org'
+license           'All Rights Reserved'
+description       'Installs/Configures osl-nextcloud'
+issues_url        'https://github.com/osuosl-cookbooks/osl-nextcloud/issues'
+source_url        'https://github.com/osuosl-cookbooks/osl-nextcloud'
+chef_version      '>= 16.0'
+version           '0.1.0'
+
+supports          'centos', '~> 7.0'
+supports          'centos_stream', '~> 8.0'
