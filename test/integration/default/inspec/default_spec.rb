4c67398a66f65956066b4b8fd587ee10887c6569
Paul Simko <paul@osuosl.org>
2022-09-13 15:54:28 -0700

    more tests

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -3,13 +3,14 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec/resources/
 control 'osl_nextcloud' do
-  describe package redis do
+  describe package 'redis' do
     it { should be_installed }
   end
   describe service 'redis' do
     it { should be_running }
   end
   describe service 'httpd' do
+    it { should be_enabled }
     it { should be_running }
   end
 
@@ -23,7 +24,12 @@ control 'osl_nextcloud' do
     its('owner') { should eq 'apache' }
   end
 
-  describe http('http://10.1.100.92/nextcloud/index.php/login') do
+  describe host('localhost') do
+    it { should be_reachable }
+    it { should be_resolvable }
+  end
+
+  describe http('http://localhost/nextcloud/index.php/login') do
     its('status') { should cmp 200 }
   end
 end
